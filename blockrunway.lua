0b1a70eae60e362ae4f842579d240c32aa328afc
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-02-07 15:03:42 +0000

    Add blockrunway.lua to load scripts and teleport player
    Load external scripts and set player position.

---
+++ blockrunway.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/headonly.lua"))()
+
+for i = 10, 0, -1 do
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/breakfreevfx.lua"))()
+end
+
+game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-63, 295, -508)
