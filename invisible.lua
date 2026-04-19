ba4436d4b611e6a05a368f567edbcb853213b966
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-04-19 17:25:06 +0100

    Create invisible.lua

---
+++ invisible.lua
@@ -0,0 +1,14 @@
+local RemoteEvents = game.ReplicatedStorage.RemoteEvents
+local equip = RemoteEvents.EquipItem
+local toggle = RemoteEvents.DressUpToggle
+
+equip:FireServer("Abaya")
+equip:FireServer("Item Of The Week: Granny Dress")
+equip:FireServer("Fur Gloves")
+equip:FireServer("Socks")
+equip:FireServer("Troll Head")
+toggle:FireServer("Abaya")
+toggle:FireServer("Item Of The Week: Granny Dress")
+toggle:FireServer("Fur Gloves")
+toggle:FireServer("Socks")
+toggle:FireServer("Troll Head")
