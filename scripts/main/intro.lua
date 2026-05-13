82fc3bb283c8fb0107a9d60a3b2f8575c9df73a7
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-13 19:16:18 +0100

    Create intro.lua

---
+++ scripts/main/intro.lua
@@ -0,0 +1,36 @@
+local notifCont
+	local success = pcall(function()
+		notifCont = require(game:GetService("ReplicatedStorage").Client.Controllers.NotificationController)
+	end)
+	if notifCont then
+		notifCont:Notify("Welcome to Starlight DTI GUI! Please read the Main tab for some info.")
+	end
+	if setclipboard then
+		setclipboard("https://discord.gg/FaN3kG55Qb")
+		if notifCont then
+			notifCont:Notify("Discord invite copied to clipboard.")
+		end
+	end
+	
+	local main = script.Parent
+	local uiScale = main:FindFirstChildOfClass("UIScale") or Instance.new("UIScale", main)
+	uiScale.Scale = 0
+	
+	task.wait(0.5)
+	
+	local tween = game.TweenService:Create(uiScale, TweenInfo.new(0.75, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Scale = 1})
+	tween:Play()
+	tween.Completed:Wait()
+	uiScale:Destroy()
+	
+	task.wait(1)
+	
+	local credits = main:WaitForChild("Container"):WaitForChild("Credits")
+	
+	local tween = game.TweenService:Create(credits, TweenInfo.new(1, Enum.EasingStyle.Linear), {TextTransparency = 1})
+	local tweenS = game.TweenService:Create(credits.UIStroke, TweenInfo.new(1, Enum.EasingStyle.Linear), {Transparency = 1})
+	tween:Play()
+	tweenS:Play()
+	tweenS.Completed:Wait()
+	
+	credits:Destroy()
