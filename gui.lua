0f515730216ed687bd08cdf78fc8e2607e3e5598
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-13 19:17:49 +0100

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -5613,42 +5613,11 @@ task.spawn(C_3);
 -- StarterGui.DTIGUI.Main.Intro
 local function C_8()
 local script = G2L["8"];
-	local notifCont
-	local success = pcall(function()
-		notifCont = require(game:GetService("ReplicatedStorage").Client.Controllers.NotificationController)
-	end)
-	if notifCont then
-		notifCont:Notify("Welcome to Starlight DTI GUI! Please read the Main tab for some info.")
-	end
-	if setclipboard then
-		setclipboard("https://discord.gg/FaN3kG55Qb")
-		if notifCont then
-			notifCont:Notify("Discord invite copied to clipboard.")
-		end
-	end
-	
-	local main = script.Parent
-	local uiScale = main:FindFirstChildOfClass("UIScale") or Instance.new("UIScale", main)
-	uiScale.Scale = 0
-	
-	task.wait(0.5)
-	
-	local tween = game.TweenService:Create(uiScale, TweenInfo.new(0.75, Enum.EasingStyle.Quad, Enum.EasingDirection.Out), {Scale = 1})
-	tween:Play()
-	tween.Completed:Wait()
-	uiScale:Destroy()
-	
-	task.wait(1)
-	
-	local credits = main:WaitForChild("Container"):WaitForChild("Credits")
-	
-	local tween = game.TweenService:Create(credits, TweenInfo.new(1, Enum.EasingStyle.Linear), {TextTransparency = 1})
-	local tweenS = game.TweenService:Create(credits.UIStroke, TweenInfo.new(1, Enum.EasingStyle.Linear), {Transparency = 1})
-	tween:Play()
-	tweenS:Play()
-	tweenS.Completed:Wait()
-	
-	credits:Destroy()
+	local module = loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/module.lua"))()
+  local f = module.getScript("main/intro.lua", script)
+  if f then
+   f()
+  end
 end;
 task.spawn(C_8);
 -- StarterGui.DTIGUI.Main.Container.CategoriesScroll.SwitchHandler
