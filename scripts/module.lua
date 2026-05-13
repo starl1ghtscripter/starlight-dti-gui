4285d931c0f079e2e6a0768d18a44e761d28fa4e
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-13 07:14:56 +0100

    Create module.lua

---
+++ scripts/module.lua
@@ -0,0 +1,7 @@
+local pref = "https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/"
+return {
+ getScript = function(path)
+  local full = pref..path
+  return loadstring(game:HttpGet("full"))
+ end
+}
