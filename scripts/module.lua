5888f03ab7408fd64ece58be312f85db043a0247
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-13 07:42:18 +0100

    Update module.lua

--- scripts/module.lua
+++ scripts/module.lua
@@ -1,6 +1,6 @@
 local pref = "https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/"
 return {
- ["getScript"] = function(path)
+ ["getScript"] = function(path, scriptObj)
   local full = pref..path
   local src = game:HttpGet(full)
 
@@ -9,7 +9,11 @@ return {
    warn("Loadstring failed:", err)
    return nil
   end
-
+  
+  if scriptObj then
+   fn = "local script = scriptObj".."\n"..fn
+  end
+  
   return fn
  end
 }
