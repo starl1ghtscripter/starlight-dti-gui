77d9ca5193655bdddc8b6f65b6600fe10df35590
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-13 19:03:40 +0100

    Create hiding.lua

---
+++ scripts/main/hiding.lua
@@ -0,0 +1,67 @@
+local mobile = false
+	local UIS = game.UserInputService
+	if UIS.TouchEnabled and not UIS.KeyboardEnabled then
+		mobile = true
+	end
+	local keybind = Enum.KeyCode.LeftShift
+	local text = script.ShiftNotif
+	local hidden = not script.Parent.Main.Visible
+	local mobileButton = script.Parent.HidingButton
+	local function newShiftText()
+		for _, v in pairs(script.Parent:GetChildren()) do
+			if v:IsA("TextLabel") and v.Name == "ShiftNotif" then
+				v:Destroy()
+			end
+		end
+		local textClone = text:Clone()
+		textClone.Parent = script.Parent
+		textClone.Visible = true
+		task.spawn(function()
+			textClone.TextTransparency = 0
+			textClone.UIStroke.Transparency = 0
+			task.wait(1)
+			local tween = game.TweenService:Create(textClone, TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {TextTransparency = 1})
+			local tween2 = game.TweenService:Create(textClone.UIStroke, TweenInfo.new(0.5, Enum.EasingStyle.Quad, Enum.EasingDirection.InOut), {Transparency = 1})
+			tween:Play()
+			tween2:Play()
+			tween2.Completed:Connect(function()
+				textClone:Destroy()
+			end)
+		end)
+	end
+	local function toggle()
+		hidden = not hidden
+		script.Parent.Main.Visible = not hidden
+	
+		if mobile then
+			mobileButton.Visible = hidden
+		else
+			if hidden then
+				newShiftText()
+			end
+		end
+	end
+	if mobile then
+		local hideButton = script.Parent.Main.Hide
+		mobileButton.Visible = hidden
+		mobileButton.Draggable = true
+		hideButton.MouseButton1Up:Connect(function()
+			toggle()
+		end)
+		mobileButton.MouseButton1Click:Connect(function()
+			toggle()
+		end)
+	else
+		mobileButton.Visible = false
+		local button = script.Parent.Main.Hide
+		text.Visible = true
+		button.MouseButton1Up:Connect(function()
+			toggle()
+		end)
+		UIS.InputBegan:Connect(function(inp, proc)
+			if proc then return end
+			if inp.KeyCode == keybind then
+				toggle()
+			end
+		end)
+	end
