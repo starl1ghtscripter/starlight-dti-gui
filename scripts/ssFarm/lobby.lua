41fbbe5f487e5f5707453a8857ec2546c66412cb
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-25 22:51:32 +0100

    Update teleporter script to load new game code

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -1,8 +1,8 @@
 return function()
 	game:GetService("ReplicatedStorage"):WaitForChild("TeleporterEvents"):WaitForChild("EnterTeleporter"):FireServer()
 	writefile("StarlightDTI-Config/ssFARM", "true")
-	local starlight = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/gui.lua"))()'
+	local code = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()'
 	game.Players.LocalPlayer.OnTeleport:Connect(function()
-		queue_on_teleport(starlight)
+		queue_on_teleport(code)
 	end)
 end
