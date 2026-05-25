25fb311cb434bdba7814aaa3adfce8e74df069d2
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-25 23:36:17 +0100

    Update lobby.lua

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -17,8 +17,4 @@ queue_on_teleport([[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
 ]])
 
-game.Players.ChildRemoved:Connect(function(plr)
-    if plr == game.Players.LocalPlayer then
-      delfile("StarlightDTI-Config/ssFARM")
-    end
-end)
+delfile("StarlightDTI-Config/ssFARM")
