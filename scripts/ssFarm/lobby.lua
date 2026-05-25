4addc1d9415cc0a6b8416b55a1d251b11b517d14
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-25 22:52:05 +0100

    Update lobby.lua

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -1,8 +1,6 @@
-return function()
-	game:GetService("ReplicatedStorage"):WaitForChild("TeleporterEvents"):WaitForChild("EnterTeleporter"):FireServer()
-	writefile("StarlightDTI-Config/ssFARM", "true")
-	local code = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()'
-	game.Players.LocalPlayer.OnTeleport:Connect(function()
-		queue_on_teleport(code)
-	end)
-end
+game:GetService("ReplicatedStorage"):WaitForChild("TeleporterEvents"):WaitForChild("EnterTeleporter"):FireServer()
+writefile("StarlightDTI-Config/ssFARM", "true")
+local code = 'loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()'
+game.Players.LocalPlayer.OnTeleport:Connect(function()
+	queue_on_teleport(code)
+end)
