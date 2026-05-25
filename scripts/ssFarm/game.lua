fce9781e126ab7f281a448884d5faef1c4e40d2b
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-25 23:02:01 +0100

    Update game.lua

--- scripts/ssFarm/game.lua
+++ scripts/ssFarm/game.lua
@@ -1,11 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/gui.lua"))()
 
-local lobbyCode = [[
-loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
-]]
+task.wait(1)
 
-game.Players.LocalPlayer.OnTeleport:Connect(function(State)
-    if State == Enum.TeleportState.Started then
-        queue_on_teleport(lobbyCode)
-    end
-end)
+queue_on_teleport([[
+loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
+]])
