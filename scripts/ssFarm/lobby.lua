2796188bdec1c8a87d7a230e0ef12c0c4a34c202
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-25 23:31:43 +0100

    Add player removal handler to lobby script

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -16,3 +16,9 @@ ReplicatedStorage:WaitForChild("TeleporterEvents")
 queue_on_teleport([[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
 ]])
+
+game.Players.ChildRemoving:Connect(function(plr)
+    if plr == game.Players.LocalPlayer then
+      delfile("StarlightDTI-Config/ssFARM")
+    end
+end)
