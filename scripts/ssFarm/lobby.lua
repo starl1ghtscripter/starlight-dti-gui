b21811946b6618bd435dfd08d8095d7c5df8a24e
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-26 10:16:50 +0100

    Update lobby.lua

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -1,12 +1,5 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
-if not isfolder("StarlightDTI-Config") then
-    makefolder("StarlightDTI-Config")
-end
-if not isfile("StarlightDTI-Config/ssFARM") then
-    writefile("StarlightDTI-Config/ssFARM", "true")
-end
-
 task.wait(5)
 
 ReplicatedStorage:WaitForChild("TeleporterEvents")
@@ -14,18 +7,6 @@ ReplicatedStorage:WaitForChild("TeleporterEvents")
     :FireServer()
 
 queue_on_teleport([[
-    if not isfolder("StarlightDTI-Config") then
-    makefolder("StarlightDTI-Config")
-end
-if not isfile("StarlightDTI-Config/ssFARM") then
-    writefile("StarlightDTI-Config/ssFARM", "true")
-end
-task.wait(1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
+    task.wait(1)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
 ]])
-
-task.wait(1)
-
-if isfile("StarlightDTI-Config/ssFARM") then
-    delfile("StarlightDTI-Config/ssFARM")
-end
