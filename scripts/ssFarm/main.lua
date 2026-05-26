5424b94b40f2117aa0d23b98bf47184d2c348b23
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-26 10:16:38 +0100

    Update main.lua

--- scripts/ssFarm/main.lua
+++ scripts/ssFarm/main.lua
@@ -1,8 +1,11 @@
 local TeleportService = game:GetService("TeleportService")
 local Players = game:GetService("Players")
 
+if not isfolder("StarlightDTI-Config") then makefolder("StarlightDTI-Config") end
+writefile("StarlightDTI-Config/farm_mode.txt", "styleshowdown")
+
 queue_on_teleport([[
-loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
 ]])
 
 TeleportService:Teleport(127076465038391, Players.LocalPlayer)
