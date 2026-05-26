363ffd3bb5571fbf488ef307c2a63d21a985157f
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-26 10:18:56 +0100

    Update game.lua

--- scripts/ssFarm/game.lua
+++ scripts/ssFarm/game.lua
@@ -1,8 +1,28 @@
+local Players = game:GetService("Players")
+
+game:GetService("CoreGui").ChildRemoved:Connect(function(child)
+	if child.Name == "Starlight" then
+		if isfile("StarlightDTI-Config/farm_mode.txt") then
+			writefile("StarlightDTI-Config/farm_mode.txt", "normal")
+		end
+	end
+end)
+
+Players.PlayerRemoving:Connect(function(player)
+	if player == Players.LocalPlayer then
+		if isfile("StarlightDTI-Config/farm_mode.txt") then
+			writefile("StarlightDTI-Config/farm_mode.txt", "normal")
+		end
+	end
+end)
+
 task.wait(5)
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/gui.lua"))()
 
-queue_on_teleport([[
-    task.wait(1)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
-]])
+if isfile("StarlightDTI-Config/farm_mode.txt") and readfile("StarlightDTI-Config/farm_mode.txt") == "styleshowdown" then
+	queue_on_teleport([[
+		task.wait(1)
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
+	]])
+end
