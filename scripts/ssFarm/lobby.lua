988495e9c4bf1c39af84537e9a39382249029f47
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-26 10:18:41 +0100

    Update lobby.lua

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -1,12 +1,31 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Players = game:GetService("Players")
+
+game:GetService("CoreGui").ChildRemoved:Connect(function(child)
+	if child.Name == "Starlight" then
+		if isfile("StarlightDTI-Config/farm_mode.txt") then
+			writefile("StarlightDTI-Config/farm_mode.txt", "normal")
+		end
+	end
+end)
+
+Players.PlayerRemoving:Connect(function(player)
+	if player == Players.LocalPlayer then
+		if isfile("StarlightDTI-Config/farm_mode.txt") then
+			writefile("StarlightDTI-Config/farm_mode.txt", "normal")
+		end
+	end
+end)
 
 task.wait(5)
 
-ReplicatedStorage:WaitForChild("TeleporterEvents")
-    :WaitForChild("EnterTeleporter")
-    :FireServer()
+if isfile("StarlightDTI-Config/farm_mode.txt") and readfile("StarlightDTI-Config/farm_mode.txt") == "styleshowdown" then
+	ReplicatedStorage:WaitForChild("TeleporterEvents")
+		:WaitForChild("EnterTeleporter")
+		:FireServer()
 
-queue_on_teleport([[
-    task.wait(1)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
-]])
+	queue_on_teleport([[
+		task.wait(1)
+		loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
+	]])
+end
