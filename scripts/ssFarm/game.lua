5c53ef96022e51e696c9942e267422e6369516fa
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-26 09:59:22 +0100

    Update game.lua

--- scripts/ssFarm/game.lua
+++ scripts/ssFarm/game.lua
@@ -10,6 +10,13 @@ task.wait(5)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/gui.lua"))()
 
 queue_on_teleport([[
+    if not isfolder("StarlightDTI-Config") then
+    makefolder("StarlightDTI-Config")
+end
+if not isfile("StarlightDTI-Config/ssFARM") then
+    writefile("StarlightDTI-Config/ssFARM", "true")
+end
+task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/lobby.lua"))()
 ]])
 
