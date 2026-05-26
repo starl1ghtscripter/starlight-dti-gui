2ac8a803522c1b9753577f38caddbf8387521acf
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-26 09:59:09 +0100

    Update lobby.lua

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -14,6 +14,13 @@ ReplicatedStorage:WaitForChild("TeleporterEvents")
     :FireServer()
 
 queue_on_teleport([[
+    if not isfolder("StarlightDTI-Config") then
+    makefolder("StarlightDTI-Config")
+end
+if not isfile("StarlightDTI-Config/ssFARM") then
+    writefile("StarlightDTI-Config/ssFARM", "true")
+end
+task.wait(1)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
 ]])
 
