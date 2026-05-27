08b1a73e33d2194afb6e0445a8400e655ae3d860
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-27 13:07:40 +0100

    Add player removal handling and file cleanup

--- scripts/ssFarm/lobby.lua
+++ scripts/ssFarm/lobby.lua
@@ -1,24 +1,26 @@
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
+game.Players.PlayerRemoving:Connect(function(plr)
+	if plr == game.Players.LocalPlayer then
+		if isfile("StarlightDTI-Config/ssFARM") then
+			delfile("StarlightDTI-Config/ssFARM")
+		end
+	end
+end)
+
 if not isfolder("StarlightDTI-Config") then
-    makefolder("StarlightDTI-Config")
+	makefolder("StarlightDTI-Config")
 end
 if not isfile("StarlightDTI-Config/ssFARM") then
-    writefile("StarlightDTI-Config/ssFARM", "true")
+	writefile("StarlightDTI-Config/ssFARM", "true")
 end
 
 task.wait(5)
 
 ReplicatedStorage:WaitForChild("TeleporterEvents")
-    :WaitForChild("EnterTeleporter")
-    :FireServer()
+	:WaitForChild("EnterTeleporter")
+	:FireServer()
 
 queue_on_teleport([[
 loadstring(game:HttpGet("https://raw.githubusercontent.com/starl1ghtscripter/starlight-dti-gui/refs/heads/main/scripts/ssFarm/game.lua"))()
 ]])
-
-task.wait(1)
-
-if isfile("StarlightDTI-Config/ssFARM") then
-    delfile("StarlightDTI-Config/ssFARM")
-end
