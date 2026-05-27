0c0a247e6ea93296b18e2e5853f4af0d225e56d7
R <71460861+starl1ghtscripter@users.noreply.github.com>
2026-05-27 13:17:53 +0100

    Update game.lua

--- scripts/ssFarm/game.lua
+++ scripts/ssFarm/game.lua
@@ -1,7 +1,14 @@
+local teleport = false
+game.Players.LocalPlayer.OnTeleport:Connect(function()
+	teleport = true
+end)
+
 game.Players.PlayerRemoving:Connect(function(plr)
 	if plr == game.Players.LocalPlayer then
-		if isfile("StarlightDTI-Config/ssFARM") then
-			delfile("StarlightDTI-Config/ssFARM")
+		if not teleport then
+			if isfile("StarlightDTI-Config/ssFARM") then
+				delfile("StarlightDTI-Config/ssFARM")
+			end
 		end
 	end
 end)
